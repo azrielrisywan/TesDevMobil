2280ac9b6b6e4bf214a527f5e415d29ad62ea1f4
azrielrisywan <azrielrisywan@gmail.com>
2022-06-30 13:08:38 +0700

    splash

--- app/src/main/java/id/azriel/tesdevmobil/LoginActivity.kt
+++ app/src/main/java/id/azriel/tesdevmobil/LoginActivity.kt
@@ -14,7 +14,7 @@ class LoginActivity: AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonLogin.setOnClickListener {
+        binding.loginButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
